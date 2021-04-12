7169812b6fb57554091cd273c054c0286428f122
pires-sobreira <gabriel.pires-sobreira@ensta-paris.fr>
2021-04-12 22:51:44 +0200

    working simulation

--- voiture-Autonome-2020-2021/code/adaptevitesserelat.py
+++ voiture-Autonome-2020-2021/code/adaptevitesserelat.py
@@ -25,7 +25,16 @@ def adaptevitesserelat (Mm1,M,MMMR,alpha,v,deltat,rmax):
     while i<nnn:
         theta=mmmr[i][0]
         theta=int(theta)
-        vrel=(M[theta][1]-Mm1[int((acos(v*deltat/M[theta][1]/(1-cos(theta*2*pi/360)/max(sin(theta*2*pi/360),0.01))))*360/(2*pi))][1])/deltat #approx de la derivee de la distance a l'objet par rapport au temps"   ATTENTION REMPLACER (orientation-orientationm1) par les donnees de l'accelerometre dorientation/dt*deltat
+        print("M[theta][1]: ", M[theta][1])
+        print("cos(theta*2*pi/360: ", cos(theta*2*pi/360))
+        a = v*deltat/M[theta][1]/(1-cos(theta*2*pi/360)/max(sin(theta*2*pi/360),0.01))
+        print("a antigo: ", a)
+        if a > 1:
+            a = 1
+        if a < -1:
+            a = -1
+        print("a novo: ", a)
+        vrel=(M[theta][1]-Mm1[int((acos(a))*360/(2*pi))][1])/deltat #approx de la derivee de la distance a l'objet par rapport au temps"   ATTENTION REMPLACER (orientation-orientationm1) par les donnees de l'accelerometre dorientation/dt*deltat
         
         thetap=atan(mmmr[i][5]/mmmr[i][4]) #angle de coordonnees polaires du pt p attention c'est bien en angle cette fois"
         rp=sqrt(mmmr[i][5]**2+mmmr[i][4]**2) #rayon de coordonnees polaires du pt p"
